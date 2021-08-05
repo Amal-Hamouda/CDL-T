760c4fbfee198a41ddc41ea4141c2c5083769b0f
Ahmed-Mannai <ahmedmannai487@gmail.com>
2021-08-05 13:22:11 +0100

    merge changes

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'https://test.centre-des-lumieres.fr/CDL-T/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
--- application/views/donate.php
+++ application/views/donate.php
@@ -1,5 +1,5 @@
 <?php
-require_once('vendor/autoload.php');
+require_once('/vendor/stripe/stripe-php/init.php');
 \Stripe\Stripe::setApiKey('sk_test_51JHpHoG89vko2fx6LOAAOiW6g8NBU4dfXh8QbKULTHMs8b7UJwDQXkkS18RsyTk8JJ5mS3bP3KQ3WZ41Bz51YfWC00nEi4ookx');
 $session =\Stripe\Checkout\Session::create([
   'payment_method_types' => ['card'],
