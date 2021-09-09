57af04da3120496bd96552e75ac18a8fb78f05da
Ahmed-Mannai <ahmedmannai487@gmail.com>
2021-09-09 10:50:22 +0100

    excel test

--- application/controllers/Excel_export.php
+++ application/controllers/Excel_export.php
@@ -16,10 +16,13 @@ class Excel_export extends CI_Controller {
     $objPHPExcel = new PHPExcel();
     $objPHPExcel->setActiveSheetIndex(0);
     // set Header
-    $objPHPExcel->getActiveSheet()->SetCellValue('A1', 'nom');
-    $objPHPExcel->getActiveSheet()->SetCellValue('B1', 'prenom');
-    $objPHPExcel->getActiveSheet()->SetCellValue('C1', 'id');
-    $objPHPExcel->getActiveSheet()->SetCellValue('D1', 'datejour');
+    $objPHPExcel->getActiveSheet()->SetCellValue('A1', 'First Name');
+    $objPHPExcel->getActiveSheet()->SetCellValue('B1', 'Last Name');
+    $objPHPExcel->getActiveSheet()->SetCellValue('C1', 'ID');
+    $objPHPExcel->getActiveSheet()->SetCellValue('D1', 'Email');
+    $objPHPExcel->getActiveSheet()->SetCellValue('E1', 'Phone number');
+    $objPHPExcel->getActiveSheet()->SetCellValue('F1', 'Message');
+    $objPHPExcel->getActiveSheet()->SetCellValue('G1', 'Date');
 
     // set Row
     $rowCount = 2;
@@ -27,7 +30,10 @@ class Excel_export extends CI_Controller {
         $objPHPExcel->getActiveSheet()->SetCellValue('A' . $rowCount, $list->nom);
         $objPHPExcel->getActiveSheet()->SetCellValue('B' . $rowCount, $list->prenom);
         $objPHPExcel->getActiveSheet()->SetCellValue('C' . $rowCount, $list->id);
-        $objPHPExcel->getActiveSheet()->SetCellValue('D' . $rowCount, $list->datejour);
+        $objPHPExcel->getActiveSheet()->SetCellValue('D' . $rowCount, $list->email);
+        $objPHPExcel->getActiveSheet()->SetCellValue('E' . $rowCount, $list->phone);
+        $objPHPExcel->getActiveSheet()->SetCellValue('F' . $rowCount, $list->msg);
+        $objPHPExcel->getActiveSheet()->SetCellValue('G' . $rowCount, $list->datejour);
           $rowCount++;
     }
     $filename = "tutsmake". date("Y-m-d-H-i-s").".csv";
