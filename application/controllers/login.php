1c4c369ecc238983c197fa507eb79886313e804f
Ahmed-Mannai <ahmedmannai487@gmail.com>
2021-09-10 15:00:19 +0100

    login

---
+++ application/controllers/login.php
@@ -0,0 +1,57 @@
+<?php
+class login extends CI_Controller {
+
+public function index()
+{
+    
+  $this->load->view('login');
+
+}
+function validation()
+ {
+    $this->load->library('form_validation');
+  $this->form_validation->set_rules('user_email', 'Email Address', 'required|trim|valid_email');
+  $this->form_validation->set_rules('user_password', 'Password', 'required');
+  if($this->form_validation->run())
+  {
+
+   $email = $this->input->post('user_email');
+   $password = $this->input->post('user_password');
+   $this->db->where('email', $email);
+   $query = $this->db->get('login');
+   if($query->num_rows() > 0)
+   {
+    foreach($query->result() as $row)
+    {
+ 
+    
+      if($password == $row->password)
+      {
+       $this->session->set_userdata('id', $row->id);
+       $this->load->view('affichageformcontact');
+      }
+      else
+      {
+       
+        $this->session->set_flashdata('message','Mot de passe erroné');
+        $this->load->view('login');
+      }
+    
+    }
+   }
+   else
+   {
+   
+    $this->session->set_flashdata('message','email erroné');
+    $this->load->view('login');
+  
+   }
+  
+
+
+  }
+  else
+  {
+   $this->index();
+  }
+ }
