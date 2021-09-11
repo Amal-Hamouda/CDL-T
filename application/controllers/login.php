23d1dfe45da74ec0045782f7d44b965cd244a8df
Ahmed-Mannai <ahmedmannai487@gmail.com>
2021-09-11 13:17:53 +0100

    fix login

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -1,6 +1,13 @@
 <?php
 class login extends CI_Controller {
 
+     public function __construct() {
+           parent::__construct();
+           $this->load->library("session");
+           $this->load->helper('url');
+        }
+         
+
 public function index()
 {
     
--- application/controllers/loginadmin.php
+++ application/controllers/loginadmin.php
@@ -3,6 +3,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class loginadmin extends CI_Controller {
 
+	public function __construct() {
+		parent::__construct();
+		$this->load->library("session");
+		$this->load->helper('url');
+	 }
+	  
+
 	/**
 	 * Index Page for this controller.
 	 *
