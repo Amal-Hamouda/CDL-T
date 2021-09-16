797e104ef307dcb214cfbaaa528c3fbffac68238
Ahmed-Mannai <ahmedmannai487@gmail.com>
2021-09-16 11:11:52 +0100

    donate payment fix

--- application/views/donate.php
+++ application/views/donate.php
@@ -19,7 +19,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
     <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
 	<link rel="stylesheet" href="<?php echo base_url();?>/resources/css/donate.css" />
-	<script src="https://checkout.stripe.com/checkout.js"></script>
+	<script src="https://js.stripe.com/v3/"></script>
 
 
     <!-- web-font loader-->
