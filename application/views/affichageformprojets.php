e727c984373bb54a682378267b132c6ca38e1156
Ahmed-Mannai <ahmedmannai487@gmail.com>
2021-10-04 10:12:11 +0100

    fix

--- application/views/affichageformprojets.php
+++ application/views/affichageformprojets.php
@@ -21,6 +21,8 @@
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>resources/css/main.css">
     <link href="<?php echo base_url();?>resources/css/styles.css" rel="stylesheet" />
 <!--===============================================================================================-->
+<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"> </script> 
 </head>
 <body>
 <div class="d-flex" id="wrapper">
