739ac9275d6f6ae00cdf1b552d6c8e5465057e25
Ahmed-Mannai <ahmedmannai487@gmail.com>
2021-10-07 09:33:23 +0100

    Fix excel 2

--- application/views/affichageformprojets.php
+++ application/views/affichageformprojets.php
@@ -192,9 +192,7 @@ thead:last-child tr:last-child th:first-child, tbody:last-child tr:last-child td
   </tr>
 
 </table>
-<div style="text-align:center">
-                        <button id="downloadexcel" class="btn btn-success">Export to excel</button>
-   </div>                    
+            
    
             </div>
         </div>
@@ -218,16 +216,6 @@ thead:last-child tr:last-child th:first-child, tbody:last-child tr:last-child td
 			
 		
 	</script>
-<!--===============================================================================================-->
-<script src="<?php echo base_url();?>resources/js/table2excel.js"></script>
-
-<script>
-    document.getElementById("downloadexcel").addEventListener('click', function(){
-        var table2excel = new Table2Excel();
-  table2excel.export(document.querySelectorAll("#table"), "Tableau projets");
-    });
- 
-</script>
 <!--===============================================================================================-->
 	<script src="<?php echo base_url();?>resources/js/main.js"></script>
     <script src="<?php echo base_url();?>resources/js/scripts.js"></script>
