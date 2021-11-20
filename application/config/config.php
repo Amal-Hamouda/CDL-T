b07a501fd40520dfd0f242102e07867d8d4a8ad8
Amal-Hamouda <amall.hammouda@gmail.com>
2021-11-20 18:35:02 +0100

    fix replacing base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own...
 |
 */
-$config['base_url'] = 'https://test.centre-des-lumieres.fr/CDL-T';
+$config['base_url'] = 'https://test.centre-des-lumieres.fr/CDL/CDL-T';
 
 /*
 |--------------------------------------------------------------------------
